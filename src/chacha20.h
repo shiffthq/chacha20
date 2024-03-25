19fe5c840e5406e36f59a5ec0a601ae2f58c9431
Rosmianto Saputro <rosmianto.site@gmail.com>
2024-03-25 14:51:11 +0700

    Add C++ macro

--- src/chacha20.h
+++ src/chacha20.h
@@ -1,7 +1,16 @@
 #ifndef __CHACHA20_H
 #define __CHACHA20_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdint.h>
 
 void ChaCha20XOR(uint8_t key[32], uint32_t counter, uint8_t nonce[12], uint8_t *input, uint8_t *output, int inputlen);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
